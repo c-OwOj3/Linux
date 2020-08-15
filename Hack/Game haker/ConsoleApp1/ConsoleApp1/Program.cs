b8b52d24b4da9f9d62fdd709c4f6d584ce580d5a
c-OwOj3 <c-OwOj3@github.com>
2020-08-15 20:33:32 +0800

    me!me!me!
    good  start

---
+++ Hack/Game haker/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static int Base = 0x00509B74;
+        public static int Health = 0xF8;
+
+        static void Main(string[] args)
+        {
+
+            VAMemory vam = new VAMemory("ac_client");
+            int Localplayer = vam.ReadInt32((IntPtr)Base);
+
+            while (true) {
+
+                int address = Localplayer + Health;
+                vam.WriteInt32((IntPtr)address, 888);
+            }
+            
+        }
+    }
+}
